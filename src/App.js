598ffa2db32fcdbaf23fc5b878766bdc5106a5ae
Xinyang Li <xili@indeed.com>
2018-11-27 14:53:50 +0900

    Clean project structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Singapore Landlord List </h1>
+        <p>
+          A list that tracks the behaviour of landlords in Singapore, good and bad, operating in public with full
+          transparency.
+        </p>
+
+        <p>
+          Want to update this list or have any question? Please create an{" "}
+          <a href="https://github.com/sglandlordlist/sglandlordlist.github.io/issues/new">issue</a>. We try to keep the
+          list as close to the fact as possible but there is no guarantee.
+        </p>
+        <strong>
+          No account? Register <a href="https://github.com/join">here</a>.
+        </strong>
+        <strong>
+          Want to add more features to this page, check out its{" "}
+          <a href="https://github.com/sglandlordlist/sglandlordlist.github.io">source code</a> on github.
+        </strong>
+        <h3>Read the following guidelines before creating an issue.</h3>
+        <ul>
+          <li>Fact, only fact and full fact.</li>
+          <li>
+            Keep both tenant's and landlord's privacy in mind. No personal identity info e.g. no full name, no full
+            phone number. Mask personal inforation in attached images.
+          </li>
+          <li>Stay objective and polite.</li>
+        </ul>
+        <p>Issues not following the guidelines need to get updated otherwise will be deleted.</p>
       </div>
     );
   }
